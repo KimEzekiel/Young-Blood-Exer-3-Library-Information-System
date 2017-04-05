541d3b8fa6b841464af9e8f1c500af149fc18019
KimEzekiel <kldelmundo@up.edu.ph>
2017-04-05 15:30:28 +0800

    Library with setters and getters

--- src/Book.java
+++ src/Book.java
@@ -1,56 +1,51 @@
 public class Book{
-  //attributes
-  private String title;
-  private String id; // must be unique, decimal
-  private String author;
-  private int yearPublished;
-  private String type; // could be "textbook","novel","dictionary","encyclopedia"
+	private String id;
+	private String title;
+	private String author;
+	private int year;
+	private String type;
 
-  //constants
+	public Book(String id, String title, String author,int year,String type){
+		this.id= id;
+		this.title = title;
+		this. author = author;
+		this.year= year;
+		this.type=type;
+	}
+	String getId(){
+		return this.id;
+	}
+	String getTitle(){
+		return this.title;
+	}
+	String getAuthor(){
+		return this.author;
+	}
+	int getYear(){
+		return this.year;
+	}
+	String getType(){
+		return this.type;
+	}
+	void setId(String id){
+		this.id = id;
+	}
+	void setTitle(String title){
+		this.title = title;
+	}
+	void setAuthor(String author){
+		this.author = author;
+	}
+	void setYear(int year){
+		this.year = year;
+	}
+	void setType(String type){
+		this.type = type;
+	}
 
-  //constructor
-  public Book(String title, String author, int yearPublished, String type){
-    this.title = title;
-    this.id;//di pa to tapos
-    this.author = author;
-    this.yearPublished = yearPublished;
-    this.type = type;
-  }
-
-  //methods
 
 
-  //setters
-  public void setTitle(String title){
-    this.title = title;
-  }
-  public void setID(String id){
-    this.id = id;
-  }
-  public void setAuthor(String author){
-    this.author = author;
-  }
-  public void setYearPublished(int yearPublished){
-    this.yearPublished = yearPublished;
-  }
-  public void setType(String type){
-    this.type = type;
-  }
 
-  //getters
-  public String getTitle(){
-    return this.title;
-  }
-  public String getID(){
-    return this.id;
-  }
-  public String getAuthor(){
-    return this.author;
-  }
-  public int getYearPublished(){
-    return this.yearPublished;
-  }
-  public String getType(){
-    return this.type;
-  }
+
+	
 }
--- src/Library.java
+++ src/Library.java
@@ -1,7 +1,15 @@
+//import Book;
+import java.util.ArrayList;
+import java.util.HashMap;
 public class Library{
-	private HashMap<String, Book>;
+	private HashMap<String, ArrayList> bookCollection;
+
 	public Library(){
+		bookCollection = new HashMap<String, ArrayList>();
 	}
 
+	public void addBook(Book newBook){
+		//newBook.getTitle()
+	}
 
 }
