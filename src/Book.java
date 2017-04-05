987b8b0cc7ab5736fe7eeb2178538476db41f447
KimEzekiel <kldelmundo@up.edu.ph>
2017-04-06 02:11:35 +0800

    save for library

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,7 @@
 package library;
 
+import java.util.ArrayList;
+
 public class Book{
 	private String id;
 	private String title;
@@ -51,5 +53,5 @@ public class Book{
 
 
 
-	
+
 }
--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main{
     //view borrowed books in library
     //save borrowedBooks before exit
     //save library hashmap before exit
+    
 
   }
 }
